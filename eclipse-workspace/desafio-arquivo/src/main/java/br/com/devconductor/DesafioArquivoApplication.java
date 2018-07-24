b883f8148cd6bd779ccc21fb399aaa146b0496d5
nutellatux <rodrigo.lucena83@outlook.com>
2018-07-23 23:48:58 -0300

    Primeiro commit

---
+++ eclipse-workspace/desafio-arquivo/src/main/java/br/com/devconductor/DesafioArquivoApplication.java
@@ -0,0 +1,12 @@
+package br.com.devconductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioArquivoApplication.class, args);
+	}
+}
---
+++ eclipse-workspace/desafio-arquivo/src/test/java/br/com/devconductor/DesafioArquivoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.devconductor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioArquivoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
